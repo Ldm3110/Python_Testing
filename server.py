b89779ec506c9ab112517ede96405eae83ce8dcf
Ldm3110 <ludovic.dematteis@icloud.com>
2022-01-04 10:59:08 +0100

    fixes : club cannot book more than it has points

--- server.py
+++ server.py
@@ -58,6 +58,10 @@ def purchase_places():
         # It's impossible to reserve more than 12 places in a same tournament
         message = "You cannot reserve more than 12 places sorry !!"
         return render_template('booking.html', club=club, competition=competition, message=message)
+    elif places_required > club['points']:
+        # Club does not have enough points
+        message = f"You don't have enough points ! You can reserve {club['points']} places maximum"
+        return render_template('booking.html', club=club, competition=competition, message=message)
     else:
         competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - places_required
         flash('Great-booking complete!')
--- tests/unit_test/test_booking.py
+++ tests/unit_test/test_booking.py
@@ -10,3 +10,15 @@ class TestPurchase:
         response = client.post('/purchasePlaces', data=data, follow_redirects=True)
         response_decode = response.data.decode()
         assert "You cannot reserve more than 12 places sorry !!" in response_decode
+
+    def test_user_cannot_reserve_more_than_he_has_points(self, client, test_clubs,
+                                                         test_competitions, testing_config):
+        test_clubs[0]['points'] = 4
+        data = {
+            "club": test_clubs[0]["name"],
+            "competition": test_competitions[0]["name"],
+            "places": 10,
+        }
+        response = client.post('/purchasePlaces', data=data, follow_redirects=True)
+        response_decode = response.data.decode()
+        assert "have enough points" in response_decode
